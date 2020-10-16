65ed36d0cc327cd426490aebb37d05640692b832
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-10-16 08:59:33 +0530

    Add files via upload

---
+++ Bill Generation_1/src/main/java/main/java/yaksha/SnacksDetails.java
@@ -0,0 +1,19 @@
+package main.java.yaksha;
+
+public class SnacksDetails {
+	 public double generateBill(int pizza, int puff, int drink) {
+		
+       /* int tp=pizza*100,tf,td;
+        tf=puff*20;
+        td=drink*10;
+        double total=tp+tf+td;
+        System.out.println("Bill Details");
+        System.out.println("No of pizzas:"+pizza);
+        System.out.println("No of puffs:"+puff);
+        System.out.println("No of cooldrinks:"+drink);
+       
+		return total;*/
+		 return 0;
+	}
+
+}
